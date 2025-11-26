c96fae57220ae0ce3ad5e75109cd18bd20d3c87f
Exoleton <contact.exoleton@gmail.com>
2025-11-26 21:19:44 +0100

    Reorder navbar search next to CTA

--- index.php
+++ index.php
@@ -75,14 +75,14 @@ $currentUser = current_user($pdo);
           </li>
           <li class="nav-item"><a class="nav-link" href="#comparateur" data-i18n="nav.comparator">Comparateur</a></li>
           <li class="nav-item"><a class="nav-link" href="#guides" data-i18n="nav.guides">Guides</a></li>
+          <li class="nav-item ms-lg-3">
+            <a class="nav-link" href="#cta" data-i18n="nav.cta">Découvrir les solutions</a>
+          </li>
           <li class="nav-item">
             <a class="nav-link nav-search-highlight d-flex align-items-center gap-1" href="recherche.php">
               <span data-i18n="nav.search">Recherche</span>
               <span class="search-icon" aria-hidden="true">🔍</span>
             </a>
-          </li>
-          <li class="nav-item ms-lg-3">
-            <a class="nav-link" href="#cta" data-i18n="nav.cta">Découvrir les solutions</a>
           </li>
           <li class="nav-item ms-lg-3">
             <label class="visually-hidden" for="languageSwitcher" data-i18n="lang.label">Langue</label>
